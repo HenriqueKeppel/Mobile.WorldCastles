46f21f713eb5a04afadfd0f3948fbdb548dd498d
Henrique Franzoni Keppel <416459@sga.pucminas.br>
2019-05-28 22:11:16 -0300

    Incluindo service-worker

---
+++ sources/service-worker.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Nome do cache
+const CACHE_NAME = 'static-cache-v1';
+
+// TODO: descomentar depois
+ const FILES_TO_CACHE = [
+//     'images/offline.svg',
+//     'offline_001.html',
+ ];
+ 
+self.addEventListener('install', (evt) => {
+    console.log('[ServiceWorker] Instalando');
+ 
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Fazendo cache da página estática');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+ 
+    self.skipWaiting();
+});
